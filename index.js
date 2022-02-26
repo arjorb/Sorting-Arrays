63474c102e765c585647ab34086ab8e3be6c5aa2
arjorb <arjorb@gmail.com>
2022-02-26 11:23:34 +0200

    adding the feature to sort out the fruits orange into orage shelf and apple into apple shelf

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ let appleEl = document.getElementById('apple-el');
 let orageEl = document.getElementById('orange-el');
 
 for(let i = 0; i < fruit.length; i++){
-    
+    if(fruit[i] === "🍎"){
+        appleEl.textContent += fruit[i]
+    }else{
+        orageEl.textContent += fruit[i]
+    }
 }
