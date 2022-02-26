16b07451c5a48c24e2b64c8f40a892ae3605bb9c
arjorb <arjorb@gmail.com>
2022-02-26 11:19:39 +0200

    adding the DOM to the index.js file

---
+++ index.js
@@ -0,0 +1,8 @@
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"];
+
+let appleEl = document.getElementById('apple-el');
+let orageEl = document.getElementById('orange-el');
+
+for(let i = 0; i < fruit.length; i++){
+    
+}
